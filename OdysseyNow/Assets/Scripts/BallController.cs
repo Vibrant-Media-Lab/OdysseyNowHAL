ad43eadaf12d6e8fd2b17781d12a752b5dcc40f8
Griftor05 <glynch1@cps.edu>
2018-10-07 18:33:24 -0400

    Card 1 Successfully implemented. I'm not in love with the vertical movement model of the ball, but it is entirely functional.

--- OdysseyNow/Assets/Scripts/BallController.cs
+++ OdysseyNow/Assets/Scripts/BallController.cs
@@ -31,42 +31,55 @@ public class BallController : MonoBehaviour {
 	void FixedUpdate () {
         // Speed is a function of the current position on the screen, running from the full speed value to some fraction of full speed at min
         // Makes a modifier as a fraction of the screen, and ensures that 1 <= modifier <= 0.5 (so we never stop)
-        float speed_modifier = (gameObject.transform.position.x / Screen.width) / 2 + 0.5f;
+        // Do it as an offset from the left so that the coordinates are always positive
+        float coordinate_offset_from_left = gameObject.transform.position.x + Screen.width;
+        float speed_modifier = (coordinate_offset_from_left / Screen.width) / 2 + 0.5f;
         // If player 2 has possession of the ball, we have to flip it
-        if (ballpossesssion == possession.PLAYER2) speed_modifier = 1 - speed_modifier;
+        if (ballpossesssion == possession.PLAYER2) speed_modifier = speed_modifier * -1;
         // If BOTH players have possession, need to set it so that it will go to the center
-        // TO BE IMPLEMENTED LATER
+        // Do this by just enabling both behaviours
+        if (ballpossesssion == possession.BOTH) {
+            if (gameObject.transform.position.x > 0) speed_modifier = speed_modifier * -1;
+            else if (Mathf.Abs(gameObject.transform.position.x) < 0.05) speed_modifier = 0;
+        }
 
         float speed = full_speed * speed_modifier;
         float newx = gameObject.transform.position.x + speed;
-        if (Mathf.Abs(newx) > 7) newx = 7;
+        if (newx > 7) newx = 7;
+        if (newx < -7) newx = -7;
         // Also need to implement it going to the English of P1 and P2
         float tgty = 0f;
         if (ballpossesssion == possession.PLAYER1) tgty = player1EnglishTarget.transform.position.y;
         if (ballpossesssion == possession.PLAYER2) tgty = player2EnglishTarget.transform.position.y;
+        if (ballpossesssion == possession.BOTH) tgty = (player1EnglishTarget.transform.position.y + player2EnglishTarget.transform.position.y) / 2;
         float oldy = gameObject.transform.position.y;
+
+        // Closes distance in the same way the playerspots do
+        // Though tbh this doesn't match console behaviour very well, so we'll have to fix it up a little
         float newy = (tgty - oldy) / ball_english_lag + gameObject.transform.position.y;
         if (Mathf.Abs(newy - oldy) < 0.05) newy = oldy;
+       
         // Now scoot along, little ball
-
         gameObject.transform.position = new Vector3(newx, newy, gameObject.transform.position.z);
 
         // Also need to check for key presses
-        // Each just set the state - they don't actually perform any logic beyond thatdd
-        if (Input.GetKey(resetbuttonPlayer1) && Input.GetKey(resetbuttonPlayer2)) ballpossesssion = possession.BOTH;
-        else if (Input.GetKey(resetbuttonPlayer1)) ballpossesssion = possession.PLAYER1;
-        else if (Input.GetKey(resetbuttonPlayer2)) ballpossesssion = possession.PLAYER2;
+        // Each just set the state - they don't actually perform any logic beyond that
+        if (Input.GetKeyDown(resetbuttonPlayer1) && Input.GetKeyDown(resetbuttonPlayer2))
+            ballpossesssion = possession.BOTH;
+        else if (Input.GetKeyDown(resetbuttonPlayer1)) ballpossesssion = possession.PLAYER1;
+        else if (Input.GetKeyDown(resetbuttonPlayer2)) ballpossesssion = possession.PLAYER2;
+
+        Debug.Log(ballpossesssion);
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(collision.gameObject.name);
         // Basically when you run into the player, you turn around and the player takes possession
-        if (collision.gameObject.name == "Player1Body")
+        if (other.gameObject.name == "Player1Body")
         {
             ballpossesssion = possession.PLAYER1;
         }
-        else if (collision.gameObject.name == "Player2Body") {
+        else if (other.gameObject.name == "Player2Body") {
             ballpossesssion = possession.PLAYER2;
         }
     }
