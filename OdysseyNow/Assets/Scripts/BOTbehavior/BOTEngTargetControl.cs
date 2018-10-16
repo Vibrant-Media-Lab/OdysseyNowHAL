3552cf991b7fba081a2b7b2d7fb89840e6fc1873
Jiang <furry.bian@gmail.com>
2018-10-15 20:09:45 -0400

    Script for Bots
    Very dumb AI that cannot be defeated.

---
+++ OdysseyNow/Assets/Scripts/BOTbehavior/BOTEngTargetControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOTEngTargetControl : MonoBehaviour {
+
+    public float startx, starty;
+    static float targetY;
+    public float speed;
+    public GameObject opponent;
+    public GameObject ball;
+
+    void Start () {
+        gameObject.transform.position = new Vector3(startx, starty,0);
+        targetY = 0;
+        StartCoroutine(RandomEng());
+    }
+
+	void Update ()
+    {
+        //smoothly move the target
+        if (targetY - gameObject.transform.position.y > 0.1)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + speed * Time.deltaTime, 0);
+        }
+        else if (targetY - gameObject.transform.position.y < -0.1)
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - speed * Time.deltaTime, 0);
+        }
+    }
+
+    //Randomly select a spot every 0.2 sec
+    IEnumerator RandomEng()
+    {
+        while (true)
+        {
+            //no ridiculous movement
+            targetY = Random.Range(-8, 8);
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+    
+}
---
+++ OdysseyNow/Assets/Scripts/BOTbehavior/BOTTargetControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOTTargetControl : MonoBehaviour {
+    public float startx, starty;
+    public GameObject ball;
+    // Use this for initialization
+    void Start () {
+        gameObject.transform.position = new Vector3(startx, starty,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float y = ball.transform.position.y;
+        gameObject.transform.position = new Vector3(gameObject.transform.position.x, y, gameObject.transform.position.z);
+	}
+}
