27a2cb96c8cf6c2a619abdbc0712381f3e3aca95
Brian Popeck <bpopeck4@gmail.com>
2019-01-29 00:17:07 -0500

    Add functionality for menu options toggling game objects and moving the wall

--- OdysseyNow/Assets/Scripts/MenuDirector.cs
+++ OdysseyNow/Assets/Scripts/MenuDirector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuDirector : MonoBehaviour
 {
@@ -14,5 +15,25 @@ public class MenuDirector : MonoBehaviour
     public void UpdateVariables(){
         i++;
         print("Updated variables " + i);
+    }
+
+    // toggle the active state of gameObject
+    public void ToggleGameObject(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    // need to have method per GameObject because buttons can only call functions with one or zero parameters -_-
+    // maybe you can create a utility class with multiple parameters and pass that in as the only argument?
+    public void MoveWallHorizontally(Slider slider)
+    {
+        GameObject wall = GameObject.Find("Wall");
+        wall.transform.position = new Vector3(slider.value, wall.transform.position.y, wall.transform.position.z);
+    }
+
+    public void MoveWallVertically(Slider slider)
+    {
+        GameObject wall = GameObject.Find("Wall");
+        wall.transform.position = new Vector3(wall.transform.position.x, slider.value, wall.transform.position.z);
     }
 }
