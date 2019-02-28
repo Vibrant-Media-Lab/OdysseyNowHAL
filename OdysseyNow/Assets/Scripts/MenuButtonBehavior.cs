16249782e7ef8c50d899d535dadd0bb359c6030d
EnglishLab <englishlab@463LAB07.english.pitt.edu>
2019-02-28 18:09:31 -0500

    Up to first release

---
+++ OdysseyNow/Assets/Scripts/MenuButtonBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonBehavior : MonoBehaviour
+{
+    public bool load;
+    public GameObject otherMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClicked);
+    }
+
+
+
+    void ButtonClicked(){
+        if(load){
+            SceneManager.LoadScene(gameObject.name.Replace(" ", ""));
+        }
+        else if(otherMenu != null){
+            otherMenu.SetActive(true);
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+}
