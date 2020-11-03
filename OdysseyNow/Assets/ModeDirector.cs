752d1f8671299ee7d477f4e38f8de7795dc350ae
Nathan Withers <nathan@thewithz.tech>
2020-11-03 17:33:35 -0500

    Resolving errors

--- OdysseyNow/Assets/ModeDirector.cs
+++ OdysseyNow/Assets/ModeDirector.cs
@@ -14,6 +14,7 @@ public class ModeDirector : MonoBehaviour {
                 calibration.GetComponent<CalibrationDirectorNew>().afterCalibration.AddListener(StartCatAndMouse);
                 break;
             case "Super Cat and Mouse":
+                calibration.GetComponent<CalibrationDirectorNew>().afterCalibration.AddListener(StartSuperCatAndMouse);
                 break;
             case "Haunted House":
                 break;
@@ -50,6 +51,31 @@ public class ModeDirector : MonoBehaviour {
     }
 
     private void StartCatAndMouse() {
+        calibration.SetActive(false);
+        var p1 = ElementSettings.FindFromNameAndTag("PlayerTarget", "Player1");
+        var p2 = ElementSettings.FindFromNameAndTag("PlayerTarget", "Player2");
+
+        var p1Input = (LocalInputManager.ControlScheme) System.Enum.Parse(typeof(LocalInputManager.ControlScheme),
+                                                                          PlayerPrefs.GetString("P1Input"));
+        var p2Input = (LocalInputManager.ControlScheme) System.Enum.Parse(typeof(LocalInputManager.ControlScheme),
+                                                                          PlayerPrefs.GetString("P2Input"));
+        if (p1Input == LocalInputManager.ControlScheme.AI) {
+            p1.GetComponent<NavMeshAgent>().enabled = true;
+            p1.GetComponent<CatAI>().enabled = true;
+            p1.GetComponent<CatAI>().level = PlayerPrefs.GetInt("ai1");
+        }
+
+        if (p2Input == LocalInputManager.ControlScheme.AI) {
+            p2.GetComponent<NavMeshAgent>().enabled = true;
+            p2.GetComponent<MouseAI>().enabled = true;
+            p2.GetComponent<MouseAI>().level = PlayerPrefs.GetInt("ai2");
+        }
+
+        if (p1Input == LocalInputManager.ControlScheme.AI || p2Input == LocalInputManager.ControlScheme.AI)
+            AI.SetActive(true);
+    }
+
+    private void StartSuperCatAndMouse() {
         calibration.SetActive(false);
         var p1 = ElementSettings.FindFromNameAndTag("PlayerTarget", "Player1");
         var p2 = ElementSettings.FindFromNameAndTag("PlayerTarget", "Player2");
